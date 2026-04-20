45c108bd46b8f7499f54606555a426b4e71ca9ee
mo3012199 <goldenuniverse2021@gmail.com>
2026-04-19 21:42:16 -0400

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyADfKERzKPOsrlqGHTBAkE9jwLhcrQO4Dg",
+  authDomain: "autoshop-pro-65426.firebaseapp.com",
+  projectId: "autoshop-pro-65426",
+  storageBucket: "autoshop-pro-65426.firebasestorage.app",
+  messagingSenderId: "309078800124",
+  appId: "1:309078800124:web:3dbbcfc90efc679cef8979"
+};
